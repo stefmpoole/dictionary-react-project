c9ac935027e5a75587faba7fb13e1e529946df77
stefmpoole <104706141+stefmpoole@users.noreply.github.com>
2022-09-01 15:51:09 -0400

    added search form to Dictionary
    added form, useState, added card to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
-
 import Dictionary from "./Dictionary";
+
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <main>
-          <Dictionary />
-        </main>
+        <Card>
+          <Card.Header style={{ margin: 20 }}>
+            <Card.Title>Dictionary</Card.Title>
+          </Card.Header>
+          <main>
+            <Dictionary />
+          </main>
+        </Card>
+        <footer className="App-footer">
+          <small>Coded by Stefanie Poole</small>
+        </footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,32 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition...`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
-      <p>
-        Hello World
-        <Button className="btn btn-primary">Hello World</Button>
-      </p>
+      <Form onSubmit={search}>
+        <InputGroup className="mb-3" onChange={handleKeywordChange}>
+          <Form.Control
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Text id="basic-addon2">Search</InputGroup.Text>
+        </InputGroup>
+      </Form>
     </div>
   );
 }
