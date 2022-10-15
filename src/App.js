cd1166179763d23c0587e79c401b957b682efee6
stefmpoole <104706141+stefmpoole@users.noreply.github.com>
2022-10-14 22:39:59 -0400

    updated css
    changed background and margin

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
-
 import Card from "react-bootstrap/Card";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="mb-0">Dictionary</h1>
         <Card
           className="rounded pl-3 pr-3 pt-2"
           style={{ borderColor: "white" }}
         >
+          <h1 className="mb-3">Dictionary</h1>
           <main>
             <Dictionary defaultKeyword="dictionary" />
           </main>
+          <footer className="App-footer">
+            <small>
+              Coded by Stefanie Poole and is {""}
+              <a href="https://github.com/stefmpoole/dictionary-react-project">
+                open-sourced on GitHub
+              </a>
+            </small>
+          </footer>
         </Card>
-        <footer className="App-footer">
-          <small>
-            Coded by Stefanie Poole and is {""}
-            <a href="https://github.com/stefmpoole/dictionary-react-project">
-              open-sourced on GitHub
-            </a>
-          </small>
-        </footer>
       </div>
     </div>
   );
